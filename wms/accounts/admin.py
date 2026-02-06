732c967a3a9371a2bd7c14f81b6fb1b35c52fac5
SamirBagirzade <samir.bagirzade2@gmail.com>
2026-02-07 02:02:39 +0400

    asd

--- wms/accounts/admin.py
+++ wms/accounts/admin.py
@@ -1,1 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User, Group
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("username", "email", "first_name", "last_name")
+    filter_horizontal = ("groups", "user_permissions")
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    filter_horizontal = ("permissions",)
--- wms/settings.py
+++ wms/settings.py
@@ -74,7 +74,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = "en"
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Baku"
 USE_I18N = True
 USE_TZ = True
 
