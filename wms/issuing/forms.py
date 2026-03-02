bb5293ea2189dd45946c2bb6b15bb9ab6713ba7f
SamirBagirzade <samir.bagirzade2@gmail.com>
2026-03-02 12:24:47 +0400

    fix(issue): include unit metadata on item select options for prefilled lines

--- wms/issuing/forms.py
+++ wms/issuing/forms.py
@@ -2,6 +2,22 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 from django.forms import inlineformset_factory
 from .models import IssueHeader, IssueLine
+from wms.masters.models import Item
+
+
+class ItemSelectWithUnit(forms.Select):
+    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
+        option = super().create_option(name, value, label, selected, index, subindex=subindex, attrs=attrs)
+        raw_value = option.get("value")
+        if raw_value not in (None, "", "None"):
+            try:
+                item_unit = Item.objects.filter(pk=int(raw_value)).values_list("unit", flat=True).first()
+            except (TypeError, ValueError):
+                item_unit = None
+            if item_unit:
+                option.setdefault("attrs", {})
+                option["attrs"]["data-unit"] = item_unit
+        return option
 
 
 class IssueHeaderForm(forms.ModelForm):
@@ -35,6 +51,7 @@ class IssueLineForm(forms.ModelForm):
         model = IssueLine
         fields = ["item", "qty"]
         labels = {"item": _("Item"), "qty": _("Qty")}
+        widgets = {"item": ItemSelectWithUnit()}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
